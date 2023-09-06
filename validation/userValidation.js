6c6412a077d902d9079742450f12f2f323ff64b6
muteweb <muteweb.yaseen@gmail.com>
2023-09-06 18:21:45 +0530

    small changes

--- validation/userValidation.js
+++ validation/userValidation.js
@@ -8,6 +8,13 @@ const schemas = {
         password: Joi.string().pattern(new RegExp('[A-Za-z0-9]$')).min(5).max(15),
         mobile: Joi.string().min(10).max(10).required()
     }),
+    Update: Joi.object().keys({
+        first_name: Joi.string().optional(),
+        last_name: Joi.string().optional(),
+        email: Joi.string().email().optional(),
+        password: Joi.string().optional(),
+        mobile: Joi.string().min(10).max(10).optional()
+    }),
     Login: Joi.object().keys({
         email: Joi.string().email().required(),
         password: Joi.string().required()
